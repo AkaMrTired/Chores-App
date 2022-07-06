9236435261f14b849ee26d257331d327d16807cf
Sam Michael <Samuel.Michael92@yahoo.com>
2022-07-06 20:26:48 +0100

    added components and routing

--- chores-app/src/components/App.js
+++ chores-app/src/components/App.js
@@ -1,11 +1,24 @@
 import '../styles/App.css';
 import './server.js';
+import { BrowserRouter as Router, Routes ,Route } from 'react-router-dom';
 import HomePage from './HomePage';
+import Login from './Login';
+import SignUp from './SignUp';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Router>
+        <Routes>
+
+          <Route path="/" element={<HomePage />} />
+
+          <Route path="/login" element={<Login />} />
+
+          <Route path="signup" element={<SignUp />} />
+
+        </Routes>
+      </Router>
     </div>
   );
 }
--- chores-app/src/components/HomePage.js
+++ chores-app/src/components/HomePage.js
@@ -2,7 +2,7 @@ const HomePage = () => {
     return (
         <div>
             <h1>
-                Hello World
+                Home Page
             </h1>
         </div>
     )
