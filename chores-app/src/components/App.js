184a02aaab6ec2ad89743df44da4384ac128f675
Sam Michael <Samuel.Michael92@yahoo.com>
2022-07-06 13:48:59 +0100

    clean up files & checking if it displayed

---
+++ chores-app/src/components/App.js
@@ -0,0 +1,13 @@
+import '../styles/App.css';
+import './server.js';
+import HomePage from './HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ chores-app/src/components/HomePage.js
@@ -0,0 +1,11 @@
+const HomePage = () => {
+    return (
+        <div>
+            <h1>
+                Hello World
+            </h1>
+        </div>
+    )
+}
+
+export default HomePage;
