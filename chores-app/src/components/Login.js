c65e3f8c03f60ced2b376a03d1626f66d3dee71f
Lydia Wallace <lydia.wallace@outlook.com>
2022-07-08 14:34:48 +0100

    login form work to create controlled component

--- chores-app/src/components/Login.js
+++ chores-app/src/components/Login.js
@@ -1,20 +1,47 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const initialState = {
+    fields: {
+      name: "",
+      password: "",
+    },
+  };
+  const [fields, setFields] = useState(initialState.fields);
+
+  const logIn = () => {
+    // some axios code to go here to send the field data to the database & verify it!
+    // currently the fields will reset but we can change this so we change the page to be the parent's home page once verification completes
+    setFields(initialState.fields);
+  };
+  const handleFieldChange = (event) => {
+    event.preventDefault();
+    setFields({ ...fields, [event.target.name]: event.target.value });
+  };
   return (
     <div className="container">
       <h1>Login</h1>
-      <form id="login">
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <label>
-          Password:
-          <input type="text" password="password" />
-        </label>
-        <input type="submit" value="Log in" />
+      <form onSubmit={logIn}>
+        <label htmlFor="name">Full Name </label>
+        <input
+          name="name"
+          required
+          type="text"
+          placeholder="e.g. Joan Smith"
+          value={fields.name}
+          onChange={handleFieldChange}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          required
+          placeholder="*******"
+          value={fields.password}
+          onChange={handleFieldChange}
+        />
+        <button type="submit">Log in</button>
       </form>
     </div>
   );
