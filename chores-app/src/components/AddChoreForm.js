3f1a0b776bde2e81b5073548ea2aaecb5b401867
Lydia Wallace <lydia.wallace@outlook.com>
2022-07-13 20:25:58 +0100

    add chore form done

--- chores-app/src/components/AddChoreForm.js
+++ chores-app/src/components/AddChoreForm.js
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from "react";
+import "../styles/AddChoreForm.css";
 
 const AddChoreForm = () => {
   const initialState = {
@@ -14,10 +15,28 @@ const AddChoreForm = () => {
     event.preventDefault();
     setFields({ ...fields, [event.target.name]: event.target.value });
   };
+  const handleStatusSelect = (event) => {
+    event.preventDefault();
+    setFields({ ...fields, status: event.target.value });
+  };
 
-  const handleSubmit = () => {
+  const [successMessage, setSuccessmessage] = useState("");
+
+  const handleSubmit = (event) => {
     // need to add functionality to send fields to the DB
-    console.log(fields);
+    //    axios
+    // .post("localhost:3300/chores", fields)
+    // .then((response) => {
+    //   console.log(response.status);
+    // })
+    // .catch(() => {
+    //   console.log(404);
+    // });
+    event.preventDefault();
+    setSuccessmessage(
+      `Success, you have added a chore. You can make another, or return to your dashboard`
+    );
+    setFields(initialState.fields);
   };
 
   return (
@@ -34,7 +53,7 @@ const AddChoreForm = () => {
             value={fields.name}
             onChange={handleFieldChange}
           />
-          <label htmlFor="price">Chore price</label>
+          <label htmlFor="price">Chore price in pounds</label>
           <input
             type="text"
             name="price"
@@ -43,18 +62,32 @@ const AddChoreForm = () => {
             value={fields.price}
             onChange={handleFieldChange}
           />
-          {/* <label htmlFor="price">Chore price</label>
-          <input
-            type="radio"
-            name="price"
-            required
-            placeholder="e.g. 5"
-            value={fields.price}
-            onChange={handleFieldChange}
-          /> */}
+          <p>Select if your chore will be available or unavailable to begin</p>
+
+          <button
+            type="button"
+            className={fields.status === "A" ? "selected" : ""}
+            name="available"
+            value="A"
+            onClick={handleStatusSelect}
+          >
+            Available
+          </button>
+
+          <button
+            type="button"
+            name="Unavailable"
+            className={fields.status === "U" ? "selected" : ""}
+            value="U"
+            onClick={handleStatusSelect}
+          >
+            Unavailable
+          </button>
+
           <button type="submit">Save chore</button>
         </form>
       </div>
+      <div>{successMessage && successMessage}</div>
       <div>
         <button type="button">
           <a href="/parentdashboard">Back To Dashboard</a>
--- chores-app/src/components/ChoreCard.js
+++ chores-app/src/components/ChoreCard.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-// not all props required
+// not all props required all the time
 import React from "react";
 import PropTypes from "prop-types";
 
