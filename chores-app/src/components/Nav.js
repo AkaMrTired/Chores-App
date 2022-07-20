89ec8238f6168f79b2dab8742015b95fdf3ebe85
Lydia Wallace <lydia.wallace@outlook.com>
2022-07-20 16:39:23 +0100

    added navigate to home or dashboard to the app logo in top left

--- chores-app/src/components/Nav.js
+++ chores-app/src/components/Nav.js
@@ -1,16 +1,34 @@
 import React from "react";
 import "../styles/Nav.css";
+import { useNavigate } from "react-router-dom";
 import logo from "../images/dosh-logo.png";
 import { useUserAuth } from "../context/UserAuthContext";
 import LogOut from "./LogOut";
 
 const Nav = () => {
+  const navigate = useNavigate();
   const { user } = useUserAuth();
+
+  const userRole = localStorage.getItem("userRole");
+
+  const navigateHome = (event) => {
+    event.preventDefault();
+    if (!user) {
+      navigate("/");
+    } else if (userRole === '"parent"') {
+      navigate("/parentdashboard");
+    } else {
+      navigate("/childdashboard");
+    }
+  };
+
   return (
     <div className="nav-container">
       <div id="nav-bar" className="nav-bar">
         <div className="logo-container">
-          <img src={logo} alt="app logo" className="logo" />
+          <button type="button" onClick={navigateHome}>
+            <img src={logo} alt="app logo" className="logo" />
+          </button>
         </div>
         <div className="nav-links-container active">{user && <LogOut />}</div>
       </div>
